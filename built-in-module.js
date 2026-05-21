58e67fda0c3f44bf752782c1bc82f27ef9564dd8
Sarthak Roy <p-2066-19@dpspatna.com>
2026-05-21 12:16:25 +0530

    Add on PATH and OS built-in-modules

--- built-in-module.js
+++ built-in-module.js
@@ -3,3 +3,32 @@
 //this file are OS, HTTPS, FS and PATH
 
 // OS MODULE
+const os = require('os');
+
+//Finding our user info
+const user = os.userInfo();
+console.log(user);
+console.log(`Hello How can I help you ${user.username}`);
+
+//Finding out the system uptime
+console.log(`The system uptime is ${(os.uptime())/60} minutes`);
+
+//Creating an OS Object
+const currentOS = {
+    name: os.type(),
+    release : os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
+}
+console.log(currentOS);
+
+//PATH MODULE
+
+const path = require('path');
+
+console.log(path.sep); // Will print the URL separator , which is different for different platforms
+console.log(path.join(__dirname, '/Hello/')); // It will print the URL with the arguments by cleaning it and using its separator
+console.log(path.basename(__dirname))
+
+const absolute = path.resolve(__dirname,__filename , '/variables.js//');
+console.log(absolute);
