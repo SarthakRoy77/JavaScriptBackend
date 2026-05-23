5c53b1c888eb0bc6b387a0e2a76a762439541922
Sarthak Roy <p-2066-19@dpspatna.com>
2026-05-23 12:18:03 +0530

    Important notes on npm.js and add unversioned files

---
+++ npm.js
@@ -0,0 +1,13 @@
+// NPM - Node Package Manager. Few of the important commands of the Node Package Manager are :-
+// npm -- global command, comes with node
+//local dependency - use it in this particular project
+// npm i <packageName>
+
+//global dependency - use it in any project
+// npm install -g <packageName>
+
+//package.json - manifest file its stores important things about the project/package
+//There are two methods to this - the manual approach, the commands approach
+// npm init (step by step, press enter to skip)
+// npm init -y ( everything default)
+
