ba5332326fe14f3a1e33b9c452ddb24d41512874
Sarthak Roy <p-2066-19@dpspatna.com>
2026-05-26 10:20:55 +0530

    add notes in npm.js

--- npm.js
+++ npm.js
@@ -11,3 +11,15 @@
 // npm init (step by step, press enter to skip)
 // npm init -y ( everything default)
 
+//Importance of package.json
+//Package.json is very important if it is a public project because you cannot push big node modules
+// as there is a limit to it in GitHub, but if we have a package.json we only need to run npm install, and it
+// will automatically install all the dependencies, and you will be ready to execute the code .
+
+//Nodemon is a module that watches paths and actively changes the outputs rather than waiting for re- execution
+
+console.log("Hello World!");
+console.log("I printed this just right now");
+
+//The scripts option in package.json enable us to create scripts that execute some lines of commands that can be used
+// for testing / logging outputs etc.
