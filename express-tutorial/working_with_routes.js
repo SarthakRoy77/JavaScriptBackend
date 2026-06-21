30f8e05df1dee9fbb0e406ee8c77a209cd6ee6b7
Sarthak Roy <p-2066-19@dpspatna.com>
2026-06-21 16:50:43 +0530

    add on POST and PUT http methods

--- express-tutorial/working_with_routes.js
+++ express-tutorial/working_with_routes.js
@@ -10,6 +10,9 @@ let posts = [
     {id:2, message:'Hello World 2!'},
     {id:3, message:'Hello World 3!'},
 ];
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
 
 //Query String (req.query)
 app.get('/api/posts', (req, res) => {
@@ -34,6 +37,35 @@ app.get('/api/posts/:id', (req, res) => {
     } else {
         res.status(200).send(posts.filter((post) => post.id === id))
     }
+});
+
+//Posting data with request body data
+app.post('/api/posts', (req, res) => {
+    const newPost = {
+        id: posts.length + 1,
+        message: req.body.title};
+
+    if (!newPost.message) {
+        res.status(400).send({msg : "Please include a message"});
+    }
+
+    posts.push(newPost);
+    res.send(posts);
+
+})
+
+app.put('/api/posts/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) {
+        res.status(404).json(`The id: ${id} is not valid and cannot be found`);
+    }
+    else {
+        post.message= req.body.title;
+    }
+
+    res.status(200).send(posts)
 })
 
 app.listen(port, () => {console.log("Listening on port 8000")})
