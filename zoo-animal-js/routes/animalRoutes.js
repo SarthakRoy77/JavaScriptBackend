038d0233289b6a80e0597fef054925146474c2af
Sarthak Roy <p-2066-19@dpspatna.com>
2026-07-18 16:12:18 +0530

    Add more animal routes

--- zoo-animal-js/routes/animalRoutes.js
+++ zoo-animal-js/routes/animalRoutes.js
@@ -52,7 +52,7 @@ router.post("/addAnimal", verifyToken, async (req, res, next) => {
 // Add Retrieve Animal by ID route
 router.get('/getAnimal/:id', verifyToken, async (req, res, next) => {
     if (req.verifiedData.clear <= 2) {
-        const err = new Error("You need a higher clearance level to access this route");
+        const err = new Error("Your clearance level needs to be higher than 2 to access this route");
         err.status = 400;
         return next(err);
     }
@@ -67,6 +67,12 @@ router.get('/getAnimal/:id', verifyToken, async (req, res, next) => {
 
         const [rows] = await pool.query("SELECT * FROM animals WHERE id = ?", [id]);
 
+        if (!rows || rows.length === 0) {
+            const err = new Error("Please enter a valid ID. There is no data associated with this ID");
+            err.status = 400;
+            return next(err);
+        }
+
         res.status(201).send({
             success: true,
             result: rows[0]
@@ -76,6 +82,82 @@ router.get('/getAnimal/:id', verifyToken, async (req, res, next) => {
         next(err);
     }
 });
+
+//Add getALl animals route
+router.get('/getAnimal', verifyToken, async (req, res, next) => {
+    if (req.verifiedData.clear <= 2) {
+        const err = new Error("Your clearance level needs to be higher than 2 to access this route");
+        err.status = 403;
+        return next(err);
+    }
+
+    try {
+        const [rows] = await pool.query("SELECT * FROM animals");
+
+        if (!rows || rows.length === 0) {
+            const err = new Error("Please register a new Animal. There is no data");
+            err.status = 400;
+            return next(err);
+        }
+
+        res.status(200).send({
+            success: true,
+            result: rows
+        });
+
+    } catch (err) {
+        next(err);
+    }
+});
+
+//Add PUT route
+router.put('/putAnimal/:id', verifyToken, async (req, res, next) => {
+    if (req.verifiedData.role.toLowerCase() !== "admin") {
+        const err = new Error("You need to be a admin to access this route");
+        err.status = 403;
+        return next(err);
+    }
+    if (!parseInt(req.params.id)) {
+        const err = new Error("Please enter a valid id")
+        err.status = 400;
+        return next(err);
+    }
+
+    try {
+        const name = req.body["animalName"];
+        const scienceName = req.body["scienceName"];
+        const origin = req.body["origin"];
+
+        if (!name) {
+            const err = new Error("Please enter the name of the animal");
+            err.status = 400;
+            return next(err);
+        }
+
+        if (!scienceName) {
+            const err = new Error("Please enter the scientific name of the animal");
+            err.status = 400;
+            return next(err);
+        }
+
+        if (!origin) {
+            const err = new Error("Please enter the origin of the animal");
+            err.status = 400;
+            return next(err);
+        }
+
+        await pool.query("UPDATE animals SET name = ?, scienceNamee = ?, origin = ? WHERE id =?", [name, scienceName, origin, parseInt(req.params.id)]);
+        const [rows] = await pool.query("SELECT * FROM animals WHERE id = ?", [parseInt(req.params.id)]);
+
+        res.status(200).send({
+            success: true,
+            result: rows[0]
+        });
+
+    } catch (err) {
+        next(err);
+    }
+})
 
 router.use(errorHandler);
 
